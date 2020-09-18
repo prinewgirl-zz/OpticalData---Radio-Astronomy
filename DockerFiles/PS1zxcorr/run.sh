41da426279c814d0176aa20a8aa8f97ed9d081d1
Priscila Gutierres <priscila.gutierres@gmail.com>
2020-09-18 13:08:01 -0300

    basic working files

---
+++ DockerFiles/PS1zxcorr/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export PYTHONPATH=/PS1zxcorr:$PYTHONPATH
+
+exec "$@"
---
+++ pycode/matchpixel.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 08:25:01 2020
+
+@author: priscila
+"""
+from astropy.coordinates import SkyCoord
+from astropy.coordinates import match_coordinates_sky
+from astropy.table import Table
+import os
+import sys
+  
+gama = Table.read(sys.argv[1])
+path_dir = sys.argv[2]
+path_new = '/home/priscila/devel/mestrado/results'
+error = float(sys.argv[3])
+
+def matching_index(base_catalog_ra, base_catalog_dec, catalog_ra, catalog_dec,
+                   error):
+   
+    c = SkyCoord(ra=base_catalog_ra, dec=base_catalog_dec, unit="deg")
+    catalog = SkyCoord(ra=catalog_ra, dec=catalog_dec, unit="deg")
+    idx, d2d, d3d = match_coordinates_sky(c, catalog)
+    idx = idx[d2d.arcsec < error] #error in arcsecs
+    return idx
+
+for i,filename in enumerate(os.listdir(path_dir)):
+    file_ = Table.read(os.path.join(path_dir,filename))
+    index = matching_index(gama['RA'],gama['DEC'], file_['RA'],
+                           file_['DEC'], error)
+    #test if the list is not empty
+#   #print(index)
+    if len(index) != 0:
+        table = Table(names = file_.colnames)
+        for j in index:
+            table.add_row(file_[j])
+            #print("one line written")
+        table.write(os.path.join(path_new,filename))
