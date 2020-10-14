cd1ab46a22ef133e0fda82cb982286def28bf646
Priscila Gutierres <priscila.gutierres@gmail.com>
2020-10-14 14:36:13 -0300

    enhancement
    Now it uses a more efficient algorithm to match and join two different tables.

--- pycode/matchpixel.py
+++ pycode/matchpixel.py
@@ -1,39 +1,45 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep  4 08:25:01 2020
-
-@author: priscila
-"""
-from astropy.coordinates import SkyCoord
-from astropy.coordinates import match_coordinates_sky
-from astropy.table import Table
-import os
+from astropy.table import Table, join, hstack
+from esutil import htm
+import numpy as np
+import random
 import sys
-  
+import os
+
+def match(cat_1, cat_2, column_1, column_2, column_3, column_4,error):
+    h = htm.HTM(depth=10)
+    m1, m2, d12 = h.match(np.array(cat_1[column_1]), 
+                          np.array(cat_1[column_3]),
+                          np.array(cat_2[column_2]), 
+                          np.array(cat_2[column_4]),
+                          error, maxmatch=1)
+    
+    submatched = cat_1[m1]
+    manmatched = cat_2[m2]
+    matched = hstack([submatched, manmatched])
+      
+    return matched
+def add_randomcol(cat):
+    """Input: 
+    cat: DataFrame
+    Output:
+    DataFrame with a random column
+    """
+    cat['random'] = 1.5
+    return cat
+
+error = 0.00028
+path_new = sys.argv[3] 
 gama = Table.read(sys.argv[1])
 path_dir = sys.argv[2]
-path_new = sys.argv[3]
-error = float(sys.argv[4])
-
-def matching_index(base_catalog_ra, base_catalog_dec, catalog_ra, catalog_dec,
-                   error):
-   
-    c = SkyCoord(ra=base_catalog_ra, dec=base_catalog_dec, unit="deg")
-    catalog = SkyCoord(ra=catalog_ra, dec=catalog_dec, unit="deg")
-    idx, d2d, d3d = match_coordinates_sky(c, catalog)
-    idx = idx[d2d.arcsec < error] #error in arcsecs
-    return idx
 
 for i,filename in enumerate(os.listdir(path_dir)):
+    #print(filename)
     file_ = Table.read(os.path.join(path_dir,filename))
-    index = matching_index(gama['RA'],gama['DEC'], file_['RA'],
-                           file_['DEC'], error)
-    #test if the list is not empty
-#   #print(index)
-    if len(index) != 0:
-        table = Table(names = file_.colnames)
-        for j in index:
-            table.add_row(file_[j])
-            #print("one line written")
-        table.write(os.path.join(path_new,filename))
+    data = match(gama, file_,'RA','RA','DEC','DEC',error)
+    if len(data) != 0:
+        data.add_column(1.5, name = 'random')
+        for i in range(0, len(data)):
+            data[i]['random'] = np.random.random()
+    #if len(data['RA']) != 0:
+    filename = 'match_' + filename
+    data.write(os.path.join(path_new,filename))
